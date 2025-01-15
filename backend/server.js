9376c7d98eb064e5b9bdf51058b3067de65f9c90
Shangesh S <shangesh2006@gmail.com>
2025-01-15 11:34:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
+const express = require('express')
+const app = express()
 
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+    try {
+      await mongoose.connect("mongodb://localhost:27017/follow-along", {
+        dbName: "follow-along"
+      });
+      console.log("Connected to follow-along database");
+    } catch (err) {
+      console.error("Error connecting to the database:", err);
+    }
+  };
+  app.get('/',(req,res)=>{
+    res.send('hello')
+  })
+  
+  // Call the function to connect to the database
+  connectToDatabase();
+
+app.listen(5000, ()=>{
+    console.log("Server is running on port 5000");
+})
